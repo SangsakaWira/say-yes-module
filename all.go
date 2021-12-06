f4854416fbca078376acc79cda378f8f558773f9
Sangsaka Wira <sangsakawira@gmail.com>
2021-12-06 17:37:26 +0700

    first commit

---
+++ all.go
@@ -0,0 +1,9 @@
+package sayyesmodule
+
+func SayYes(name string) string {
+	return "Yes " + name
+}
+
+func SayNo(name string) string {
+	return "No " + name
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SangsakaWira/say-yes-module
+
+go 1.15
